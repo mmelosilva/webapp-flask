118d8def0571fa67857493176669eaece106c9ce
mmelosilva <74714890+mmelosilva@users.noreply.github.com>
2023-04-11 13:24:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Ola Mundo! .. é vida que segue!</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Fala B1 .. OpenShift é vida!</p></h1>'
 
 @app.route('/api')
 def hello():
